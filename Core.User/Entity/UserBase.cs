bac6d5fd7be68c3f251517f872ce54b11569d736
jiayizhouisme <zhoujiayi7@gmail.com>
2024-01-19 20:04:24 +0800

    test

--- Core.User/Entity/UserBase.cs
+++ Core.User/Entity/UserBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.User.Entity
+﻿namespace Core.User.Entity
 {
     public abstract class UserBase
     {
