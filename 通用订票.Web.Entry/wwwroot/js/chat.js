2c97a0f05cd8633674cbb6f24543e03b27083815
zhoujiayi <zhoujiayi7@gmail.com>
2024-01-20 16:30:23 +0800

    a

--- 通用订票.Web.Entry/wwwroot/js/chat.js
+++ 通用订票.Web.Entry/wwwroot/js/chat.js
@@ -1,7 +1,7 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/chathub", {
-    accessTokenFactory: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0Mzc1MWQxNy1lMjZiLTRlMWUtYWIyNS0yNjczYjM5ZGYzZmMiLCJuYW1lIjoiYWRtaW4iLCJ0ZW5hbnQtaWQiOiJzaGFnb3UyIiwiaWF0IjoxNzA1NDgyMDY0LCJuYmYiOjE3MDU0ODIwNjQsImV4cCI6MTcwNTkxNDA2NCwiaXNzIjoiemhvdWppYXlpIiwiYXVkIjoic3Vubnlza3kifQ.3VeG_j60Y8zrROg3VnA-J5wctsnkUA7u2zHWp3LHX2w"
+    accessTokenFactory: () => "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0Mzc1MWQxNy1lMjZiLTRlMWUtYWIyNS0yNjczYjM5ZGYzZmMiLCJuYW1lIjoiYWRtaW4iLCJ0ZW5hbnQtaWQiOiJzaGFnb3UyIiwiaWF0IjoxNzA1NDgyMDY0LCJuYmYiOjE3MDU0ODIwNjQsImV4cCI6MTcwNTkxNDA2NCwiaXNzIjoiemhvdWppYXlpIiwiYXVkIjoic3Vubnlza3kifQ.3VeG_j60Y8zrROg3VnA-J5wctsnkUA7u2zHWp3LHX2w"
 }
     ).build();
 
