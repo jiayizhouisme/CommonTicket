91b332785ef38b568fd680024fdcab97b6a8b447
zhoujiayi <zhoujiayi7@gmail.com>
2024-01-27 15:44:58 +0800

    数据库变更

--- 通用订票.RedisMQ/Entity/RabbitMQTestEntity.cs
+++ 通用订票.RedisMQ/Entity/RabbitMQTestEntity.cs
@@ -12,22 +12,22 @@ using 通用订票.Application.System.Models;
 
 namespace 通用订票.RedisMQ.Entity
 {
-    [QuickRabbitMQPlusReceive("TestRabbitMQName1")]
+    [QuickRabbitMQPlusReceive("CreateOrder")]
     public class RabbitMQTestEntity1 : IQueueEntity
     {
         public object entity;
 
-        public string name => "TestRabbitMQName1";
+        public string name => "CreateOrder";
 
         [JsonIgnore]
         public object body { get { return this.entity; } }
     }
 
-    [QuickRabbitMQPlusReceive("TestRabbitMQName2")]
+    [QuickRabbitMQPlusReceive("CloseOrder")]
     public class RabbitMQTestEntity2 : IQueueEntity
     {
         public object entity;
-        public string name { get => "TestRabbitMQName1"; }
+        public string name { get => "CloseOrder"; }
         [JsonIgnore]
         public object body { get { return this.entity; } }
     }
@@ -50,7 +50,9 @@ namespace 通用订票.RedisMQ.Entity
                 //接收队列1的消息
                 var retRec1 = await _quickRabbitMqPlus.Receive<RabbitMQTestEntity1>(async (data, msg) =>
                 {
-                    await Task.Delay(1000);
+                    _logger.LogInformation("接收到消息1");
+                    await Task.Delay(2000);
+                    
 
                     //返回true代表业务逻辑处理成功，会告知MQ这条消息已经接收成功，会从MQ队列中删除
                     //返回false代表业务逻辑处理失败，会告知MQ这条消息没有处理成功，则MQ会继续推送这条消息
@@ -66,12 +68,13 @@ namespace 通用订票.RedisMQ.Entity
                 //接收队列2的消息
                 var retRec2 = await _quickRabbitMqPlus.Receive<RabbitMQTestEntity2>(async (data, msg) =>
                 {
-                    await Task.Delay(2500);
-
+                    _logger.LogInformation("接收到消息2");
+                    await Task.Delay(2000);
+                   
 
                     //返回true代表业务逻辑处理成功，会告知MQ这条消息已经接收成功，会从MQ队列中删除
                     //返回false代表业务逻辑处理失败，会告知MQ这条消息没有处理成功，则MQ会继续推送这条消息
-                    return true;
+                    return false;
                 }, async (errMsg, msg) =>
                 {
                     await Task.Delay(3000);
@@ -79,7 +82,7 @@ namespace 通用订票.RedisMQ.Entity
                 if (!retRec2.Item1)
                 {
                 }
-                await Task.Delay(100);
+                await Task.Delay(1000);
             }
         }
     }
--- 通用订票.Web.Entry/Controllers/HomeController.cs
+++ 通用订票.Web.Entry/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Core.Queue.IQueue;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Quick.RabbitMQPlus;
 using 通用订票.Application;
 using 通用订票.Application.System.Models;
@@ -10,18 +12,23 @@ namespace 通用订票.Web.Entry.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
         private readonly IQuickRabbitMQPlus _queuePushInfo;
-        public HomeController(IQuickRabbitMQPlus _queuePushInfo)
+        public HomeController(IQuickRabbitMQPlus _queuePushInfo, ILogger<HomeController> logger)
         {
             this._queuePushInfo = _queuePushInfo;
+            _logger = logger;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
+            _logger.LogInformation("test");
             var entity1 = new RabbitMQTestEntity1();
             var entity2 = new RabbitMQTestEntity2();
-            entity1.entity = new OrderCreate() { realTenantId = "mylittle shit"};
-            _queuePushInfo.Send(entity1, "TestRouteKey1");
-            _queuePushInfo.Send(entity2, "TestRouteKey2");
+            entity1.entity = new OrderCreate() { realTenantId = "mylittle shit1"};
+            await _queuePushInfo.Send(entity1, "TestRouteKey1");
+            await _queuePushInfo.Send(entity2, "TestRouteKey2");
             return View();
         }
     }
