fee24926719089fd3e3f3babfbf56af2ddd11dc8
zhoujiayi <zhoujiayi7@gmail.com>
2024-01-28 22:17:54 +0800

    数据库选择更新

--- Core.EntityFrameWork/MultiTenantDbContext.cs
+++ Core.EntityFrameWork/MultiTenantDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Furion.DatabaseAccessor;
+﻿using Furion;
+using Furion.DatabaseAccessor;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,11 +9,32 @@ using System.Threading.Tasks;
 
 namespace Core.EntityFrameWork
 {
-    [AppDbContext("SqlConnection", DbProvider.SqlServer)]
     public class MultiTenantDbContext : AppDbContext<MultiTenantDbContext, MultiTenantDbContextLocator>
     {
         public MultiTenantDbContext(DbContextOptions<MultiTenantDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connStr = App.Configuration["ConnectionStrings:SqlConnection"];
+            if (App.Configuration["ConnectionStrings:DbType"] == "SqlServer")
+            {
+                optionsBuilder.UseSqlServer(connStr, options =>
+                {
+                    options.MigrationsAssembly("通用订票.Database.Migrations");
+                });
+            }
+            else if(App.Configuration["ConnectionStrings:DbType"] == "MySql")
+            {
+                ServerVersion sv = ServerVersion.AutoDetect(connStr);
+                optionsBuilder.UseMySql(connStr,sv, options =>
+                {
+                    options.MigrationsAssembly("通用订票.Database.Migrations");
+                });
+            }
+            
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
--- 通用订票.EntityFramework.Core/MyDefaultDbContext.cs
+++ 通用订票.EntityFramework.Core/MyDefaultDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Auth;
 using Core.EntityFrameWork;
+using Furion;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,10 +19,23 @@ namespace 通用订票.EntityFramework.Core
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connStr = GetDatabaseConnectionString();
-            optionsBuilder.UseSqlServer(connStr, options =>
+            if (App.Configuration["ConnectionStrings:DbType"] == "SqlServer")
             {
-                options.MigrationsAssembly("通用订票.Database.Migrations");
-            });
+                optionsBuilder.UseSqlServer(connStr, options =>
+                {
+                    options.MigrationsAssembly("通用订票.Database.Migrations");
+                });
+            }
+            else if (App.Configuration["ConnectionStrings:DbType"] == "MySql")
+            {
+                ServerVersion sv = ServerVersion.AutoDetect(connStr);
+                optionsBuilder.UseMySql(connStr, sv, options =>
+                {
+                    options.MigrationsAssembly("通用订票.Database.Migrations");
+                });
+            }
+
+
             base.OnConfiguring(optionsBuilder);
         }
     }
