fbca90f562418cdbc8041344a75dcb886bbc15f4
zhoujiayi <zhoujiayi7@gmail.com>
2024-02-03 20:48:57 +0800

    更新版本 删除不必要的数据

---
+++ 通用订票.EventBus/OrderEventSubscriber.cs
@@ -0,0 +1,46 @@
+﻿using Furion.DependencyInjection;
+using Furion.EventBus;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 通用订票.EventBus
+{
+    public class OrderEventSubscriber : IEventSubscriber,ISingleton
+    {
+        private readonly ILogger<OrderEventSubscriber> _logger;
+        public OrderEventSubscriber(ILogger<OrderEventSubscriber> logger)
+        {
+            _logger = logger;
+        }
+
+        [EventSubscribe("Order:BeforeCreate")]
+        public async Task BeforeCreate(EventHandlerExecutingContext context)
+        {
+            var todo = context.Source;
+            _logger.LogInformation("创建一个 ToDo：{Name}", todo.Payload);
+            await Task.CompletedTask;
+        }
+
+        [EventSubscribe("Order:AfterCreated")]
+        public async Task AfterCreated(EventHandlerExecutedContext context)
+        {
+            var todo = context.Source;
+            _logger.LogInformation("创建一个 ToDo：{Name}", todo.Payload);
+            await Task.CompletedTask;
+        }
+
+        private void CreateSucceed()
+        {
+
+        }
+
+        private void CreateFail()
+        {
+
+        }
+    }
+}
---
+++ 通用订票.EventBus/StockEventSubscriber.cs
@@ -0,0 +1,36 @@
+﻿using Furion.DependencyInjection;
+using Furion.EventBus;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 通用订票.EventBus
+{
+    public class StockEventSubscriber : IEventSubscriber,ISingleton
+    {
+        private readonly ILogger<StockEventSubscriber> _logger;
+        public StockEventSubscriber(ILogger<StockEventSubscriber> logger)
+        {
+            _logger = logger;
+        }
+
+        [EventSubscribe("Stock:CheckSucceed")]
+        public async Task StockCheckSucceed(EventHandlerExecutingContext context)
+        {
+            var todo = context.Source;
+            _logger.LogInformation("创建一个 ToDo：{Name}", todo.Payload);
+            await Task.CompletedTask;
+        }
+
+        [EventSubscribe("Stock:CheckFailed")]
+        public async Task StockCheckFailed(EventHandlerExecutedContext context)
+        {
+            var todo = context.Source;
+            _logger.LogInformation("创建一个 ToDo：{Name}", todo.Payload);
+            await Task.CompletedTask;
+        }
+    }
+}
