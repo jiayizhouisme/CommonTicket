f1b2bdf638727f2b1d6a90a36d27b69b54584d6b
zhoujiayi <zhoujiayi7@gmail.com>
2024-02-21 18:54:21 +0800

    slavedb

---
+++ Core.EntityFrameWork/Locator/SlaveDbContextLocator.cs
@@ -0,0 +1,13 @@
+﻿using Furion.DatabaseAccessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.EntityFrameWork.Locator
+{
+    public class SlaveDbContextLocator : IDbContextLocator
+    {
+    }
+}
