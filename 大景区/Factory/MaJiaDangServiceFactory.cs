25a5d748fe654abbec06511c00312d55a1eb8a9a
zhoujiayi <zhoujiayi7@gmail.com>
2024-02-21 19:56:08 +0800

    修改bug

---
+++ 大景区/Factory/MaJiaDangServiceFactory.cs
@@ -0,0 +1,30 @@
+﻿using Furion.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 通用订票.大景区.Service;
+using 通用订票.Application.System.IService.Factory;
+using 通用订票.Application.System.Services.IService;
+
+namespace 通用订票.大景区.Factory
+{
+    public class MaJiaDangServiceFactory : IServiceFactory
+    {
+        public IDefaultOrderServices GetOrderService(INamedServiceProvider<IDefaultOrderServices> _orderProvider)
+        {
+            return _orderProvider.GetService<ITransient>(nameof(MaJiaDangOrderService));
+        }
+
+        public IDefaultAppointmentService GetStockService(INamedServiceProvider<IDefaultAppointmentService> _stockProvider)
+        {
+            return _stockProvider.GetService<ITransient>(nameof(MaJiaDangStockService));
+        }
+
+        public IDefaultTicketService GetTicketService(INamedServiceProvider<IDefaultTicketService> _ticketProvider)
+        {
+            return _ticketProvider.GetService<ITransient>(nameof(MaJiaDangTicketService));
+        }
+    }
+}
---
+++ 大景区/Service/MaJiaDangOrderService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Cache;
+using Furion.DatabaseAccessor;
+using Furion.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using 通用订票.Application.System.ServiceBases.IService;
+using 通用订票.Application.System.Services.IService;
+using 通用订票.Application.System.Services.Service;
+using 通用订票Order.Entity;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+
+namespace 通用订票.大景区.Service
+{
+    [Injection(Order = 0)]
+    public class MaJiaDangOrderService : DefaultOrderService, ITransient
+    {
+        private ILogger _log { get; set; }
+
+        public MaJiaDangOrderService(IRepository<Core.Entity.Order, MasterDbContextLocator> _dal, ICacheOperation cache, ILoggerFactory logger) : base(_dal, cache)
+        {
+            _log = logger.CreateLogger("MaJiaDangOrder");
+        }
+
+        public override Task<Core.Entity.Order> CreateOrder(Guid objectId, string name, decimal amount, OrderStatus status = OrderStatus.未付款)
+        {
+            _log.Log(LogLevel.Information, "大景区订单开始下单");
+            return base.CreateOrder(objectId, name + "大景区", amount, status);
+        }
+
+    }
+}
