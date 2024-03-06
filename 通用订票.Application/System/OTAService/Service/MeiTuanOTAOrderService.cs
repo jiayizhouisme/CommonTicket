b4b80c5cf25417ac10f54b4835e071b68964c64c
jiayizhouisme <zhoujiayi7@gmail.com>
2024-03-06 14:56:40 +0800

    删除不必要的继承，增加OTA接口

---
+++ 通用订票.Application/System/OTAService/Service/MeiTuanOTAOrderService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using 通用订票.Application.System.OTAService.IService;
+using 通用订票.Application.System.Services.IService;
+using 通用订票Order.Entity;
+
+namespace 通用订票.Application.System.OTAService
+{
+    public class MeiTuanOTAOrderService : IMeiTuanOTAOrderService,ITransient
+    {
+        private IDefaultOrderServices _orderServices { get; set; }
+        public Task<Core.Entity.Order> CancelOrder(Core.Entity.Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Core.Entity.Order> PayOrder(Core.Entity.Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Core.Entity.Order> RefundOrder(Core.Entity.Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Core.Entity.Order> TakeOrder(decimal amount, OrderStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetService(IDefaultOrderServices service)
+        {
+            _orderServices = service;
+        }
+    }
+}
--- 通用订票.Application/System/ServiceBases/IService/IStockService.cs
+++ 通用订票.Application/System/ServiceBases/IService/IStockService.cs
@@ -3,7 +3,7 @@ using 通用订票.Base.IService;
 
 namespace 通用订票.Application.System.ServiceBases.IService
 {
-    public interface IStockService<T> : IBaseStockService<T>, IBaseService<T> where T : Core.BaseEntity.Stock, new()
+    public interface IStockService<T> : IBaseStockService<T>where T : Core.BaseEntity.Stock, new()
     {
         public Task<T> checkStock(Guid stockId);
         public Task DelStockFromCache(Guid stockId);
