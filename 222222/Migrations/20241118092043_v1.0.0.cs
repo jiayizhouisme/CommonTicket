6f6a611efbbb2c2d5ee3c8f9b2870cc6b7bc3b66
zhoujiayi7 <zhoujiayi7@gmail.com>
2024-11-18 17:20:47 +0800

    #

---
+++ 222222/Migrations/20241118092043_v1.0.0.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _222222.Migrations
+{
+    /// <inheritdoc />
+    public partial class v100 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StockName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exhibitions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "景区名"),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "景区介绍"),
+                    imgs = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "景区封面"),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    beforeDays = table.Column<int>(type: "int", nullable: false, comment: "景区提前预约天数"),
+                    isDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    createTime = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "创建日期"),
+                    basicPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false, comment: "价格"),
+                    passType = table.Column<int>(type: "int", nullable: false),
+                    exhibitions = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isMultiPart = table.Column<bool>(type: "bit", nullable: false),
+                    forbiddenRule = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    totalAmount = table.Column<int>(type: "int", nullable: false, comment: "一天最大预约人数")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exhibitions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TradeNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ObjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdCard = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppointmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserInfos_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    TUserId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserInfoId1 = table.Column<int>(type: "int", nullable: false),
+                    UserInfoId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppointmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_UserInfos_UserInfoId1",
+                        column: x => x.UserInfoId1,
+                        principalTable: "UserInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_AppointmentId",
+                table: "Tickets",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_OrderId",
+                table: "Tickets",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserInfoId1",
+                table: "Tickets",
+                column: "UserInfoId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInfos_AppointmentId",
+                table: "UserInfos",
+                column: "AppointmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exhibitions");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "UserInfos");
+
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
